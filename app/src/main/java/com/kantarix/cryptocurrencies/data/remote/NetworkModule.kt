c8d12407657b1a7e3bcc11d65f18968a575d574e
Flethy <petrova.vera.ev@gmail.com>
2022-09-28 13:28:20 +0300

    create network module and request permissions

---
+++ app/src/main/java/com/kantarix/cryptocurrencies/data/remote/NetworkModule.kt
@@ -0,0 +1,35 @@
+package com.kantarix.cryptocurrencies.data.remote
+
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import java.util.concurrent.TimeUnit
+
+class NetworkModule(noConnectionInterceptor: NoConnectionInterceptor) {
+
+    private val baseUrl = "https://api.coingecko.com/api/"
+    private val version = "v3/"
+
+    private val json = Json {
+        ignoreUnknownKeys = true
+    }
+
+    private val contentType = "application/json".toMediaType()
+
+    private val httpClient = OkHttpClient.Builder()
+        .connectTimeout(10, TimeUnit.SECONDS)
+        .readTimeout(20, TimeUnit.SECONDS)
+        .addInterceptor(noConnectionInterceptor)
+        .build()
+
+    private val retrofit = Retrofit.Builder()
+        .baseUrl(baseUrl + version)
+        .addConverterFactory(json.asConverterFactory(contentType))
+        .client(httpClient)
+        .build()
+
+    //val api: CoinsApiService by lazy { retrofit.create(CoinsApiService::class.java) }
+
+}
